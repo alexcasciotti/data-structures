8dffa1e773dd4f2165530f43abb3925f529a9956
Alex Casciotti <alexc2048@gmail.com>
2022-09-18 17:36:17 -0400

    Queue implementation

--- data-structures.py
+++ data-structures.py
@@ -13,6 +13,19 @@ class Stack:
         return len(self.stk) == 0
 
 class Queue:
-    pass
+    def __init__(self):
+        self.q = []
+    def enqueue(self, value):
+        self.q.append(value)
+    def dequeue(self):
+        to_remove = self.q[0]
+        self.q.remove(to_remove)
+        return to_remove
+    def peek(self):
+        return self.q[0]
+    def isEmpty(self):
+        return len(self.q) == 0
+
+
 
 
