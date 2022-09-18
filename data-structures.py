ea137d53a18a266f6ab66a396f7d9fec41478166
Alex Casciotti <alexc2048@gmail.com>
2022-09-18 17:25:34 -0400

    Stack update

--- data-structures.py
+++ data-structures.py
@@ -1,31 +1,20 @@
+from functools import total_ordering
+
+
 class Stack:
     def __init__(self):
         self.stk = []
-        self.top = -1
-        self.size = 0
     def push(self, value):
         self.stk.append(value)
-        self.top += 1
-        self.size += 1
     def pop(self):
-        self.top -= 1
-        self.size -= 1
-        return self.stk[self.top + 1]
+        to_remove = self.stk[len(self.stk) - 1]
+        self.stk.remove(to_remove)
+        return to_remove
     def peek(self):
-        return self.stk[self.top]
+        return self.stk[len(self.stk) - 1]
     def isEmpty(self):
-        return self.size == 0
-
-# stk = Stack()
-# stk.push(4)
-# print(stk.peek())
-# stk.push(3)
-# print(stk.peek())
-# stk.push(5)
-# print(stk.peek())
-# while(not stk.isEmpty()):
-#     print(stk.pop())
-
+        return len(self.stk) == 0
+        
 class Queue:
     pass
 
